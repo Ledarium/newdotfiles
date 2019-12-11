749d4bb229f47aa3c4c1c23f8bccd479d5ee9aa8
Dmitry Maylarov <maylarov@yandex.ru>
2019-12-11 13:46:00 +0300

    zsh-suggestions

--- .config_init.sh
+++ .config_init.sh
@@ -7,6 +7,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 

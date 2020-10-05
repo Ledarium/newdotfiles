9ff64f01c54c4201b88ebbc3a0e702c85ce91491
Dmitry Maylarov <maylarov@yandex.ru>
2020-10-05 21:30:30 +0300

    redshift

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo nvidia-settings --assign CurrentMetaMode="DPY-1: nvidia-auto-select @1920x1080 +3840+0 {Transform=(2.000000,0.000000,0.000000,0.000000,2.000000,0.000000,0.000000,0.000000,1.000000), ViewPortIn=3840x2160, ViewPortOut=1920x1080+0+0, ResamplingMethod=Bilinear, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DPY-2: nvidia-auto-select @3840x2160 +0+0 {ForceFullCompositionPipeline = On }"
+sudo nvidia-settings --assign CurrentMetaMode="DPY-1: nvidia-auto-select @1920x1080 +3840+0 {Transform=(2.000000,0.000000,0.000000,0.000000,2.000000,0.000000,0.000000,0.000000,1.000000), ViewPortIn=3840x2160, ViewPortOut=1920x1080+0+0, ResamplingMethod=Bilinear, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DPY-2: nvidia-auto-select @3840x2160 +0+0 {ForceFullCompositionPipeline = On }" &
 xrandr --output DP-0 --primary & 
 nm-applet &
 sleep 1
@@ -7,4 +7,5 @@ xdg-open http:// &
 telegram-desktop &
 element-desktop-nightly &
 safeeyes &
+redshift-gtk -b 1.0:0.5 -t 6500:3000 &
 
